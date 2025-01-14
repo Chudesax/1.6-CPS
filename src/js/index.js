dec4ca9831e35b1acf537732301771ad01060c26
ChudesaxWork <ilyin.ia@infotec.ru>
2025-01-14 15:45:10 +0300

    mobile swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,29 @@
 import '../scss/style.scss'
 
-console.log('Works!')
+// блок кода для кнопки читать далее в блоке main (тот, что сверху)
+
+let mobileReadMore = document.querySelector('.main__more') // Находим кнопку Читать далее
+console.log(mobileReadMore)
+let hiddenText = document.querySelector('.main__text--hidden') // Находим скрытый текст
+console.log(hiddenText)
+
+mobileReadMore.addEventListener('click', function () {
+  // вешаем обработчик событий на кнопку.
+  if (mobileReadMore.textContent === 'Читать далее') {
+    mobileReadMore.textContent = 'Скрыть' //корректируем текст
+
+    hiddenText.classList.toggle('hidden') // переключаем класс hidden
+  } else {
+    mobileReadMore.textContent = 'Читать далее'
+  }
+
+  if (mobileReadMore.textContent === 'Скрыть') {
+    mobileReadMore.classList.add('arrows__up')
+  } else {
+    mobileReadMore.classList.remove('arrows__up')
+
+    hiddenText.classList.toggle('hidden')
+  }
+})
+
+/* ----------------------------------------------------------------- */
