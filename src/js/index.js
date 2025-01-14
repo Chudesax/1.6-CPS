6829842b35a7ff0759f21e4c826574008fe3594d
Ivan Ilyin <p1@w1l.ru>
2025-01-14 20:35:17 +0300

    swiper added

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import '../scss/style.scss'
 
-// блок кода для кнопки читать далее в блоке main (тот, что сверху)
+/* блок кода для кнопки читать далее в блоке main ------------ */
 
 let mobileReadMore = document.querySelector('.main__more') // Находим кнопку Читать далее
 console.log(mobileReadMore)
@@ -18,12 +18,53 @@ mobileReadMore.addEventListener('click', function () {
   }
 
   if (mobileReadMore.textContent === 'Скрыть') {
-    mobileReadMore.classList.add('arrows__up')
+    mobileReadMore.classList.add('arrows__up') // Поворачиваем псевдострелочку
   } else {
     mobileReadMore.classList.remove('arrows__up')
 
-    hiddenText.classList.toggle('hidden')
+    hiddenText.classList.toggle('hidden') // Проверяем класс hidden
   }
 })
 
 /* ----------------------------------------------------------------- */
+
+/* Активируем свайпер для блока brands ----------------------------- */
+
+let list = document.querySelectorAll('.brands__item'); // Находим слайды
+console.log(list.length);
+
+let swiper;
+
+const swiperActivator = function () {
+  if (window.innerWidth < 768) {
+    if (!swiper) {
+        swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 1.5,
+        spaceBetween: 50,
+        breakpoints: {
+          600: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+          clickable: true,
+        },
+      });
+    }
+  } else {
+    swiper.destroy(true, true);
+    swiper = null; 
+  }
+
+};
+
+
+window.addEventListener('resize', swiperActivator);
+swiperActivator();
+
+/* -------------------------------------------------------  */
