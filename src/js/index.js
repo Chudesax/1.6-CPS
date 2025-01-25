45c96e8070ec525960ba6ed0bb77a06b11934caf
Ivan Ilyin <p1@w1l.ru>
2025-01-25 23:11:51 +0300

    FULL REPAIR

--- src/js/index.js
+++ src/js/index.js
@@ -1,83 +1,135 @@
-import '../scss/style.scss'
+import '../scss/main.scss'
 
+/* Wrapper_open/close_buttons*/
+let body = document.querySelector('.body')
+let openMobMenuButton = document.querySelector('.mob-menu__button')
+let closeMobMenuButton = document.querySelector('.side-mob-menu__button')
+let sideMenuWrapper = document.querySelector('.wrapper--side')
 
-let mobileReadMore = document.querySelector('.main__btn-more') // Находим кнопку Читать далее
-console.log(mobileReadMore)
-let hiddenText = document.querySelector('.main__text--hidden') // Находим скрытый текст
-console.log(hiddenText)
+let callWraper = document.querySelector('.wrapper-call')
+let feedbackWraper = document.querySelector('.wrapper-feedback')
+let fogRightSide = document.querySelector('.fog--right-side')
+let fogLeftSide = document.querySelectorAll('.fog--left-side')
 
-mobileReadMore.addEventListener('click', function () {
-  // вешаем обработчик событий на кнопку.
-  if (mobileReadMore.textContent === 'Читать далее') {
-    mobileReadMore.textContent = 'Скрыть' //корректируем текст
+let phoneButton = document.querySelectorAll('.button-phone')
+let chatButton = document.querySelectorAll('.button-chat')
+let closeButton = document.querySelectorAll('.button-close')
 
-    hiddenText.classList.toggle('hidden') // переключаем класс hidden
-  } else {
-    mobileReadMore.textContent = 'Читать далее'
-  }
+let scrollFlag = 0
 
-  if (mobileReadMore.textContent === 'Скрыть') {
-    mobileReadMore.classList.add('arrows__up') // Поворачиваем псевдострелочку
-  } else {
-    mobileReadMore.classList.remove('arrows__up')
-
-    hiddenText.classList.toggle('hidden') // Проверяем класс hidden
-  }
-})
-
-/* -------------------------------------------------------------- */
-
-let list = document.querySelectorAll('.brands__item') // Находим слайды
-console.log(list.length)
-let techTypesList = document.querySelectorAll('.tech-types__item') // Находим слайды
-console.log(techTypesList.length)
-let pricesList = document.querySelectorAll('.prices__item');
-console.log(pricesList);
-
-let swiper // объявляем переменную свайпер
-
-const swiperActivator = function () {
-  // Объявляем функцию активации свайпера
-  if (window.innerWidth < 767) {
-    // условие функции - срабатывает при экране меньше 768px
-    if (!swiper) {
-      // если снайпера нет - создай.
-      swiper = new Swiper('.swiper', {
-        direction: 'horizontal',
-        loop: false,
-        slidesPerView: 'auto',
-        spaceBetween: 10,
-        centeredSlides: false,
-        keyboard: {
-          enable: true,
-          onlyInViewPort: true
-        },
-        breakpoints: {
-          600: {
-            slidesPerView: 'auto',
-            spaceBetween: 20
-          },
-          425: {
-            slidesPerView: 'auto',
-            spaceBetween: 20
-          },
-          375: {
-            slidesPerView: 'auto',
-            spaceBetween: 20
-          }
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'bullets',
-          clickable: true
-        }
-      })
-    }
-  } else {
-    swiper.destroy(true, true)
-    swiper = null
+for (let i = 0; i < phoneButton.length; i++) {
+  phoneButton[i].onclick = function () {
+    callWraper.classList.replace('wrapper--closed', 'wrapper--opened')
+    body.classList.add('body--scroll')
+    scrollFlag++
   }
 }
 
-window.addEventListener('resize', swiperActivator)
-swiperActivator()
+for (let i = 0; i < chatButton.length; i++) {
+  chatButton[i].onclick = function () {
+    feedbackWraper.classList.replace('wrapper--closed', 'wrapper--opened')
+    body.classList.add('body--scroll')
+    scrollFlag++
+  }
+}
+
+openMobMenuButton.onclick = function () {
+  sideMenuWrapper.classList.replace('wrapper--closed', 'wrapper--opened')
+  body.classList.add('body--scroll')
+  scrollFlag++
+}
+
+closeMobMenuButton.onclick = function () {
+  sideMenuWrapper.classList.replace('wrapper--opened', 'wrapper--closed')
+  if (scrollFlag < 2) {
+    body.classList.remove('body--scroll')
+  }
+  scrollFlag--
+}
+
+fogRightSide.onclick = function () {
+  sideMenuWrapper.classList.replace('wrapper--opened', 'wrapper--closed')
+  if (scrollFlag < 2) {
+    body.classList.remove('body--scroll')
+  }
+  scrollFlag--
+}
+
+for (let i = 0; i < fogLeftSide.length; i++) {
+  fogLeftSide[i].onclick = function () {
+    callWraper.classList.replace('wrapper--opened', 'wrapper--closed')
+    feedbackWraper.classList.replace('wrapper--opened', 'wrapper--closed')
+    if (scrollFlag < 2) {
+      body.classList.remove('body--scroll')
+    }
+    scrollFlag--
+  }
+}
+for (let i = 0; i < closeButton.length; i++) {
+  closeButton[i].onclick = function () {
+    callWraper.classList.replace('wrapper--opened', 'wrapper--closed')
+    feedbackWraper.classList.replace('wrapper--opened', 'wrapper--closed')
+    if (scrollFlag < 2) {
+      body.classList.remove('body--scroll')
+    }
+    scrollFlag--
+  }
+}
+/*Swiper*/
+const breakpoint = window.matchMedia('(max-width:767px)')
+const enableSwiper = function () {
+  mySwiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    speed: 500,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: true
+    }
+  })
+}
+
+if (breakpoint.matches) {
+  enableSwiper()
+}
+
+/*Expand_buttons*/
+let expandButton = document.querySelectorAll('.expand-button')
+let expandText = document.querySelector('.textbox')
+let expandSection = document.querySelectorAll('.section--minimalize')
+let expandNav = document.querySelectorAll('.section-nav--minimalize')
+let expandButtonMinimalize = document.querySelectorAll(
+  '.expand-button--minimalize'
+)
+let expantButtonText = document.querySelectorAll('.expand-button__text')
+let expandButtonImg = document.querySelectorAll('.expand-button__img')
+
+for (let i = 1; i < 3; i++) {
+  expandButton[i].onclick = function () {
+    expandNav[i - 1].classList.toggle('section-nav--expanded')
+    expandSection[i - 1].classList.toggle('section--expanded')
+    expandButtonMinimalize[i - 1].classList.toggle('expand-button--expanded')
+    expandButtonImg[i].classList.toggle('expand-button__img--top')
+    if (expantButtonText[i].textContent == 'Показать все') {
+      expantButtonText[i].textContent = 'Скрыть'
+    } else {
+      expantButtonText[i].textContent = 'Показать все'
+    }
+  }
+}
+
+expandButton[0].onclick = function () {
+  expandText.classList.toggle('textbox--expanded')
+  expandButton[0].classList.toggle('expand-button--expanded')
+  expandButtonImg[0].classList.toggle('expand-button__img--top')
+  if (expantButtonText[0].textContent == 'Читать далее') {
+    expantButtonText[0].textContent = 'Скрыть'
+  } else {
+    expantButtonText[0].textContent = 'Читать далее'
+  }
+}
